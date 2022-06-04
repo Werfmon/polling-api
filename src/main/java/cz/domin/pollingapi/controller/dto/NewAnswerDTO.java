b83084f1326e9222cde88102bd1c6e0c3f7470cf
Werfmon <d.vyroubal.w@gmail.com>
2022-06-04 18:17:03 +0200

    new DTO

---
+++ src/main/java/cz/domin/pollingapi/controller/dto/NewAnswerDTO.java
@@ -0,0 +1,15 @@
+package cz.domin.pollingapi.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class NewAnswerDTO {
+    private String text;
+    private Boolean correct;
+}
