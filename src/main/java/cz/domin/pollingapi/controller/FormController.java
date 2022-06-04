d538278aa4b19ebab6a18c4aa05bf8a8ffbd37cb
Werfmon <d.vyroubal.w@gmail.com>
2022-06-04 18:11:01 +0200

    added controllers

---
+++ src/main/java/cz/domin/pollingapi/controller/FormController.java
@@ -0,0 +1,9 @@
+package cz.domin.pollingapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/form")
+public class FormController {
+}
