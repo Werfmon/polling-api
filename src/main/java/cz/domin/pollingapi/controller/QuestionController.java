1f047c5feb463d293152dd740fdcd8277edba65a
Werfmon <d.vyroubal.w@gmail.com>
2022-06-10 07:58:37 +0200

    CRUD for question

--- src/main/java/cz/domin/pollingapi/controller/QuestionController.java
+++ src/main/java/cz/domin/pollingapi/controller/QuestionController.java
@@ -1,9 +1,38 @@
 package cz.domin.pollingapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cz.domin.pollingapi.controller.dto.NewQuestionDTO;
+import cz.domin.pollingapi.controller.dto.UpdateQuestionDTO;
+import cz.domin.pollingapi.model.Question;
+import cz.domin.pollingapi.service.QuestionService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/question")
+@AllArgsConstructor
 public class QuestionController {
+    private final QuestionService questionService;
+
+    @GetMapping("/{id}")
+    public Question getQuestionById(@PathVariable Long id) {
+        return questionService.getQuestionById(id);
+    }
+    @GetMapping("/all")
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+    @PostMapping
+    public Question create(@RequestBody NewQuestionDTO newQuestionDTO) {
+        return questionService.create(newQuestionDTO);
+    }
+    @PutMapping("/{id}")
+    public Question update(@PathVariable Long id, @RequestBody UpdateQuestionDTO updateQuestionDTO){
+        return questionService.update(id, updateQuestionDTO);
+    }
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        return questionService.delete(id);
+    }
 }
--- src/main/java/cz/domin/pollingapi/service/QuestionService.java
+++ src/main/java/cz/domin/pollingapi/service/QuestionService.java
@@ -1,7 +1,42 @@
 package cz.domin.pollingapi.service;
 
+import cz.domin.pollingapi.controller.dto.NewAnswerDTO;
+import cz.domin.pollingapi.controller.dto.NewQuestionDTO;
+import cz.domin.pollingapi.controller.dto.UpdateAnswerDTO;
+import cz.domin.pollingapi.controller.dto.UpdateQuestionDTO;
+import cz.domin.pollingapi.model.Question;
+import cz.domin.pollingapi.repository.AnswerRepository;
+import cz.domin.pollingapi.repository.QuestionRepository;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class QuestionService {
+    private final QuestionRepository questionRepository;
+    private final ModelMapper modelMapper;
+
+    public Question create(NewQuestionDTO newQuestionDTO) {
+        Question question = modelMapper.map(newQuestionDTO, Question.class);
+        return questionRepository.save(question);
+    }
+    public Question getQuestionById(Long id){
+        return questionRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Question with id " + id + " not found"));
+    }
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+    public Question update(Long id, UpdateQuestionDTO updateQuestionDTO) {
+        Question question = this.getQuestionById(id);
+        question.setDescription(updateQuestionDTO.getDescription());
+        question.setTitle(updateQuestionDTO.getTitle());
+        return questionRepository.save(question);
+    }
+    public String delete(Long id) {
+        questionRepository.delete(this.getQuestionById(id));
+        return "Question was deleted successfully";
+    }
 }
